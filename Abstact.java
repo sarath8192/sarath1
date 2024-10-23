cfbdc6e5f0133049fe74c2b9ea0b1d417ee559ab
sarath8192 <154578800+sarath8192@users.noreply.github.com>
2024-10-23 15:53:30 +0530

    Create Abstact.java

---
+++ Abstact.java
@@ -0,0 +1,45 @@
+abstract class Shape
+{
+    double dim1,dim2;
+    Shape(double x,double y)
+    {
+        dim1=x;
+        dim2=y;
+    }
+    abstract double area();
+}
+class Triangle extends Shape
+{
+    Triangle(double a,double b)
+    {
+        super(a,b);
+    }
+    double area()
+    {
+        return 0.5*dim1*dim2;
+    }
+}
+class Rectangle extends Shape
+{
+    Rectangle(double i,double j)
+    {
+        super(i,j);
+    }
+    double are()
+    {
+        return dim1*dim2;
+    }
+}
+class AreaDemo
+{
+    public static void main(String args[])
+    {
+        Shape s;
+        Triangle t=new Triangle(3,4.5);
+        s=t;
+        System.out.println("Area of a triangle="+s.area());
+        Rectangle r=new Rectangle(6.5,7.5);
+        s=r;
+        System.out.println("Area of rectangle="+s.area());
+    }
+}
