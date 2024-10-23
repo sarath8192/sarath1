0f06cbe100a11d63bba39fce8195e1451c114faf
sarath8192 <154578800+sarath8192@users.noreply.github.com>
2024-10-23 15:36:17 +0530

    Create display.java
    primitive data types

---
+++ display.java
@@ -0,0 +1,24 @@
+class display
+{
+    static byte b;
+    static int i;
+    static short s;
+    static long l;
+    static float f;
+    static double d;
+    static char ch;
+
+    public static void main(String args[])
+    {
+        System.out.println("Byte value="+b);
+        System.out.println("int value="+i);
+        System.out.println("short value="+s);
+        System.out.println("long value="+l);
+        System.out.println("float value="+f);
+        System.out.println("double value="+d);
+        System.out.println("char value="+ch);
+        System.out.println("boolean value="+b);
+
+
+    }
+}
